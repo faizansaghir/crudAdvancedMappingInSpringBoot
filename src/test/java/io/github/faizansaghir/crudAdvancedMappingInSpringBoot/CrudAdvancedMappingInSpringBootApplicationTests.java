e8d42b9695fc60178fe01b6ba29a3375c605f56d
faizansaghir <dbz.rashidsaghir@gmail.com>
2024-12-05 23:17:24 +0530

    Added basic structure for project

---
+++ src/test/java/io/github/faizansaghir/crudAdvancedMappingInSpringBoot/CrudAdvancedMappingInSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.faizansaghir.crudAdvancedMappingInSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudAdvancedMappingInSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
