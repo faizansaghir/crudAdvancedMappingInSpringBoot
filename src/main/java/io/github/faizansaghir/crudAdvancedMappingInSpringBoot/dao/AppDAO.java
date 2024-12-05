e71d80c7da2d2f15b9e3e261d26ee8c514c4ce6e
faizansaghir <dbz.rashidsaghir@gmail.com>
2024-12-05 23:48:43 +0530

    Added DAO for our App

---
+++ src/main/java/io/github/faizansaghir/crudAdvancedMappingInSpringBoot/dao/AppDAO.java
@@ -0,0 +1,9 @@
+package io.github.faizansaghir.crudAdvancedMappingInSpringBoot.dao;
+
+import io.github.faizansaghir.crudAdvancedMappingInSpringBoot.entity.Instructor;
+
+public interface AppDAO {
+
+    void save(Instructor instructor);
+
+}
---
+++ src/main/java/io/github/faizansaghir/crudAdvancedMappingInSpringBoot/dao/AppDAOImpl.java
@@ -0,0 +1,22 @@
+package io.github.faizansaghir.crudAdvancedMappingInSpringBoot.dao;
+
+import io.github.faizansaghir.crudAdvancedMappingInSpringBoot.entity.Instructor;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+public class AppDAOImpl implements AppDAO{
+
+    private final EntityManager entityManager;
+
+    @Autowired
+    public AppDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Instructor instructor) {
+        entityManager.merge(instructor);
+    }
+}
