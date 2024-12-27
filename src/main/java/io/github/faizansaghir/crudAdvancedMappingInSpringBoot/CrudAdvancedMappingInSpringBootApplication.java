ab9ee3410965837cea281a899c69a80c9be84977
faizansaghir <dbz.rashidsaghir@gmail.com>
2024-12-27 16:42:41 +0530

    Tested functionality to cascade delete reviews when course is deleted

--- src/main/java/io/github/faizansaghir/crudAdvancedMappingInSpringBoot/CrudAdvancedMappingInSpringBootApplication.java
+++ src/main/java/io/github/faizansaghir/crudAdvancedMappingInSpringBoot/CrudAdvancedMappingInSpringBootApplication.java
@@ -24,8 +24,20 @@ public class CrudAdvancedMappingInSpringBootApplication {
 	public CommandLineRunner commandLineRunner(AppDAO appDAO){
 		return runner -> {
 			// createCourseAndReviews(appDAO);
-			retrieveCourseAndReviews(appDAO);
+			// retrieveCourseAndReviews(appDAO);
+			deleteCourseAndReviews(appDAO);
 		};
+	}
+
+	private void deleteCourseAndReviews(AppDAO appDAO) {
+		int id = 10;
+
+		System.out.println(STR."Deleting course with id: \{id}");
+
+		appDAO.deleteCourseById(id);
+
+		System.out.println("Done");
+
 	}
 
 	private void retrieveCourseAndReviews(AppDAO appDAO) {
