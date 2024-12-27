0b3b516a537060fa594dd4bca041ca22124844c7
faizansaghir <dbz.rashidsaghir@gmail.com>
2024-12-27 15:55:45 +0530

    Updated database reference and cleanup for implementation

--- src/main/java/io/github/faizansaghir/crudAdvancedMappingInSpringBoot/CrudAdvancedMappingInSpringBootApplication.java
+++ src/main/java/io/github/faizansaghir/crudAdvancedMappingInSpringBoot/CrudAdvancedMappingInSpringBootApplication.java
@@ -22,18 +22,7 @@ public class CrudAdvancedMappingInSpringBootApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(AppDAO appDAO){
 		return runner -> {
-			// createInstructor(appDAO);
-			// findInstructor(appDAO);
-			// deleteInstructor(appDAO);
-			// findInstructorDetail(appDAO);
-			// deleteInstructorDetail(appDAO);
-			// createInstructorWithCourses(appDAO);
-			// findInstructorWithCourses(appDAO);
-			// findInstructorWithCoursesJoinFetch(appDAO);
-			// updateInstructor(appDAO);
-			// updateCourse(appDAO);
-			// deleteInstructor(appDAO);
-			deleteCourse(appDAO);
+
 		};
 	}
 
